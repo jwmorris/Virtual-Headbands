d89bb57a385af919fc0e152266a4f09172fca148
Adam Schofield <ajschofi@ncsu.edu>
2018-01-29 19:25:46 -0500

    Added new URL configs to match product id's in the URL to a database object. If an invalid product id is entered in the url, a 404 page will display.

--- VirtualHeadbandCloset/closet/urls.py
+++ VirtualHeadbandCloset/closet/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:s_id>/', views.try_on, name='try_on')
 ]
 
 
--- VirtualHeadbandCloset/closet/views.py
+++ VirtualHeadbandCloset/closet/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+from .models import Headband
+from django.shortcuts import get_object_or_404
 
 def index(request):
     return HttpResponse("Welcome to the Headbands of Hope Virtual Closet!")
+
+def try_on(request, s_id):
+    hb = get_object_or_404(Headband, shopify_id=s_id)
+    return HttpResponse("Headband: " + hb.headband_name)
+
+
