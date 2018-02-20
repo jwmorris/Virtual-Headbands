ad6a2e53afcff81572db89d44506d28d30f5fdd3
root <root@sd-vm18.csc.ncsu.edu>
2018-02-20 13:09:15 -0500

    modified setting.py to allow Django app to be run on VM

--- VirtualHeadbandCloset/VirtualHeadbandCloset/settings.py
+++ VirtualHeadbandCloset/VirtualHeadbandCloset/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'd)r1s@zss+0t2d9a6%aouqlfc&il&k4dmfkt*8$is*li(k^wr!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['sd-vm18.csc.ncsu.edu']
 
 # Application definition
 
